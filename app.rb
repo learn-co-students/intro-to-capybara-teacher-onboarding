1e8c27491befdb6f10b4db7c3d050c5bce431fbc
Kyle Coberly <kyle.coberly@gmail.com>
2019-03-25 17:20:17 -0600

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,12 @@
 class Application < Sinatra::Base
-  # Write your code here!
+  get "/" do
+    erb :index
+  end
+
+  post "/greet" do
+    erb :greet, :locals => {
+      :name => params[:user_name]
+    }
+  end
 
 end
